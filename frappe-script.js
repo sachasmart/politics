f2495bb4dcc4c5b3a5c601031b4153efbefedfda
Sacha <sacha.smart@gmail.com>
2021-04-13 16:50:07 -0400

    update

--- frappe-script.js
+++ frappe-script.js
@@ -7,7 +7,7 @@
         dependencies: null,
         custom_class: 'director',
         description: 'Planning and Strategy with Future Majority team', 
-        resources: 'Director, Support, and Field'
+        resources: 'Director, Support Staff, Field Organizers'
     },{id: 'Task 2',
         name: 'Develop Microsite',
         start: '2021-05-10',
@@ -16,7 +16,7 @@
         dependencies: null,
         custom_class: 'director',
         description: 'Work with vendor to create microsite', 
-        resources: 'Director, Support'
+        resources: 'Director, Support Staff'
     },
     {id: 'Task 3',
         name: 'Microsite Launch',
@@ -35,7 +35,7 @@
         dependencies: null,
         custom_class: 'support',
         description: 'Email approved by Director of Special Projects and Future Majority Staff', 
-        resources: 'Support, Director'
+        resources: 'Support Staff, Director'
     },{
         id: 'approve_comms_plan',
         name: 'Approve Communications Plan',
@@ -66,7 +66,7 @@
         dependencies: 'comms_plan',
         custom_class: 'support',
         description: 'Support staff will draft a recruitment email that will be sent to incumbent candidates and candidates across Canada.', 
-        resources: 'Support'
+        resources: 'Support Staff'
     },{
         id: 'Task 5',
         name: 'Approve Recruitment Email',
@@ -96,7 +96,7 @@
         dependencies: null,
         custom_class: 'field',
         description: 'Regional Field Organizers will activate Future Majoirty Membership in their respective regions. They will also begin develop a Recruit List to solicit new Future Majority Members', 
-        resources: 'Support'
+        resources: 'Support Staff'
     },{
         id: 'Task 8',
         name: 'Outreach to 20 Incumbent Candidates',
@@ -126,7 +126,7 @@
         dependencies: 'Task 8',
         custom_class: 'support',
         description: 'Amplify incumbent videos and microsite through social media channels', 
-        resources: 'Support'
+        resources: 'Support Staff'
     }, {
         id: 'Task 16',
         name: 'Identify Swing Ridings',
@@ -136,7 +136,7 @@
         dependencies: null,
         custom_class: 'director',
         description: 'Identify swing electoral districts',
-        resources: 'Director, Support, Field'
+        resources: 'Director, Support Staff, Field Organizers'
     }, {
     id: 'Task 11',
         name: 'Identify priorities of young Canadians in respective regions',
@@ -156,7 +156,7 @@
         dependencies: 'Task 7, Task 16',
         custom_class: 'field',
         description: 'Field Organizers contact candidates in their respective regions to discuss the project and brought onboard to participate.', 
-        resources: 'Field'
+        resources: 'Field Organizers'
     },{
             id: 'Task 13',
             name: 'Seek earned media',
@@ -166,7 +166,7 @@
             dependencies: null,
             custom_class: 'director',
             description: 'Outreach to previous media contacts.',
-            resources: 'Field'
+            resources: 'Field Organizers'
         
     },{
         id: 'Task 14',
@@ -177,7 +177,7 @@
         dependencies: 'Task 13',
         custom_class: 'field',
         description: 'Collaborate with regional Future Majority Membership to generate earned regional media.',
-        resources: 'Field, Director, Support'
+        resources: 'Field Organizers, Director, Support Staff'
     },{
           id: 'Task 15',
           name: 'Obtain 1,000 1-minute videos on microsite',
@@ -187,7 +187,7 @@
           dependencies: 'Task 14',
           custom_class: 'bar-milestone',
           description: 'Successfully obtain 1,000 1-minute videos and post on microsite.',
-          resources: 'Director, Support, Field'
+          resources: 'Director, Support Staff, Field Organizers'
     },{
         
             id: 'Task 17',
@@ -198,7 +198,7 @@
             dependencies: 'Task 16',
             custom_class: 'director',
             description: 'Connect with candidates in swing electoral districts and determine their interest in participating in a (virtual) debate',
-            resources: 'Director, Support, Field'
+            resources: 'Director, Support Staff, Field Organizers'
         
     },{
 
@@ -210,7 +210,7 @@
             dependencies: 'Task 17',
             custom_class: 'bar-milestone',
             description: 'Choose 5 electoral districts and confirm their participation a debate',
-            resources: 'Director, Support, Field'
+            resources: 'Director, Support Staff, Field Organizers'
  
     },
     {
@@ -222,7 +222,7 @@
             dependencies: 'Task 18',
             custom_class: 'director',
             description: 'Coordinate debates in selected electoral districts',
-            resources: 'Director, Support, Field'
+            resources: 'Director, Support Staff, Field Organizers'
     },{
 
             id: 'Task 20',
@@ -233,7 +233,7 @@
             dependencies: 'Task 19',
             custom_class: 'director',
             description: 'Host 5 Next Gen Debates',
-            resources: 'Director, Support, Field'
+            resources: 'Director, Support Staff, Field Organizers'
     }
     
 
